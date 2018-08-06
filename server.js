839c7818629f7a258f2bb5bb70a090e49a2932b6
treemmett <treemmett@gmail.com>
2018-08-06 00:03:48 -0600

    Adds access logger

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const fs = require('fs');
+const morgan = require('morgan');
+const path = require('path');
 const routes = require('./routes');
 
 module.exports = function(){
   const app = express();
+
+  // Create write stream for logger
+  const stream = fs.createWriteStream(path.join(__dirname, 'access.log'));
+
+  // Setup logging
+  const format = ':remote-addr ":date[clf]" ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"';
+  app.use(morgan(format, {stream: stream}));
 
   // Parse application/x-www-form-urlencoded
   app.use(bodyParser.urlencoded({extended: false}));
