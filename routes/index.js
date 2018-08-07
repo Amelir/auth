9104868ee9962dde98722e80a950413859635198
treemmett <treemmett@gmail.com>
2018-08-06 20:38:38 -0600

    Adds login route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,53 @@
+const bcrypt = require('bcrypt');
 const express = require('express');
+const User = require('../schemas/user');
 
 const app = new express.Router();
 
-app.get('/', (req, res) => {
-  res.send('This is a server');
+app.post('/', (req, res, next) => {
+    // Check if required data is present
+    const requiredFields = ['email', 'password'];
+    const missingFields = [];
+  
+    requiredFields.forEach(field => {
+      if(!req.body[field]){
+        missingFields.push(field);
+      }
+    });
+  
+    // Fail if required fields are missing
+    if(missingFields.length){
+      res.status(401).send({message: 'Incorrect username or password.'});
+      return;
+    }
+
+  // Get user from database
+  User.findOne({email: req.body.email})
+    .then(model => {
+      
+      // Fail if no user was found
+      if(!model){
+        res.status(401).send({message: 'Incorrect username or password.'});
+        return;
+      }
+
+      // Compare hash
+      bcrypt.compare(req.body.password, model.hash)
+        .then(match => {
+          if(!match){
+            res.status(401).send({message: 'Incorrect username or password.'});
+            return;
+          }
+
+          // Password matched. Update login time
+          model.lastLogin = Date.now()
+          model.save();
+
+          res.end();
+        })
+        .catch(err => next(err));
+    })
+    .catch(err => next(err));
 });
 
 app.use('/register', require('./register'));
